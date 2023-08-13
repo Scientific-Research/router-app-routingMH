2ee4cab91876dd158e2fd1d64c9b27b0a097b71f
Maximilian <scientificresearch309@gmail.com>
2023-08-13 23:52:26 +0200

    all link are working but not the Post link!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import Products from "./components/products";
-import Posts from "./components/posts";
-import Home from "./components/home";
-import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
-import "./App.css";
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+import Products from './components/products';
+import Posts from './components/posts';
+import Home from './components/home';
+import Dashboard from './components/admin/dashboard';
+import ProductDetails from './components/productDetails';
+import NotFound from './components/notFound';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<div className="content">
+					<Routes>
+						<Route path="/products" Component={Products} />
+						<Route path="/posts" Component={Posts} />
+						<Route path="/admin" Component={Dashboard} />
+						<Route path="/" Component={Home} />
+					</Routes>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
